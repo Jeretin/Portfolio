8526dacf0bda7d5442c895dbeefbc3490f428ca7
JerePon <jere14p@gmail.com>
2025-03-10 16:00:47 +0200

    More pictures and infos

--- script.js
+++ script.js
@@ -16,6 +16,36 @@ const galleries = {
         {type: "image", src: "Pics/Rangers_Reserve/Tigers.jpeg", alt: "Ranger's Reserve Tigers"},
         {type: "image", src: "Pics/Rangers_Reserve/Carl.jpeg", alt: "Ranger's Reserve Carl"}
     ],
+    jere: [
+        {type: "image", src: "Pics/Jere/Jere/Norja_Jere_2.jpeg"},
+        {type: "image", src: "Pics/Jere/Jere/Jere_2.jpg"},
+        {type: "image", src: "Pics/Jere/Jere/Barca_Jere.jpg"},
+        {type: "image", src: "Pics/Jere/Jere/Barcelona_Jere.jpg"},
+        {type: "image", src: "Pics/Jere/Jere/Barcelona_1.jpg"},
+        {type: "image", src: "Pics/Jere/Jere/Barcelona_2.jpg"},
+        {type: "image", src: "Pics/Jere/Jere/Barcelona_Jere_2.jpg"},
+        {type: "image", src: "Pics/Jere/Jere/Jere_1.jpg"},
+        {type: "image", src: "Pics/Jere/Jere/Jere_3.jpg"},
+        {type: "image", src: "Pics/Jere/Jere/Levi_Jere.jpg"},
+        {type: "image", src: "Pics/Jere/Jere/Rooma.jpg"},
+        {type: "image", src: "Pics/Jere/Jere/Jere_4.jpg"},
+        {type: "image", src: "Pics/Jere/Jere/Jere_5.jpg"},
+        {type: "image", src: "Pics/Jere/Jere/Jere_6.jpg"},
+        {type: "image", src: "Pics/Jere/Jere/Jere_7.jpg"},
+        {type: "image", src: "Pics/Jere/Jere/Levi_Jere_2.jpg"},
+        {type: "image", src: "Pics/Jere/Jere/Norja_Jere.jpg"},
+        {type: "image", src: "Pics/Jere/Jere/Rooma_Jere.jpg"},
+        {type: "image", src: "Pics/Jere/Jere/Jere_8.jpg"},
+        {type: "image", src: "Pics/Jere/Jere/Zakynthos.jpg"},
+        {type: "image", src: "Pics/Jere/Jere/Zakynthos_Jere.jpg"},
+        {type: "image", src: "Pics/Jere/Jere/Jere_9.jpg"},
+        {type: "image", src: "Pics/Jere/Jere/Jere_10.jpg"},
+        {type: "image", src: "Pics/Jere/Jere/Jere_11.jpg"},
+        {type: "image", src: "Pics/Jere/Jere/Ruotsi_Jere.jpg"},
+        {type: "image", src: "Pics/Jere/Jere/Zakynthos_Jere_2.jpg"},
+        {type: "image", src: "Pics/Jere/Jere/Zakynthos_Jere_4.jpg"},
+        {type: "image", src: "Pics/Jere/Jere/Zakynthos_Jere_3.jpg"},
+    ],
 }
 let currentGallery = [];
 let currentImage = 0;
@@ -39,28 +69,29 @@ function changeMedia(direction) {
 }
 
 function updateModalContent() {
-        const modalContent = document.getElementById('modalContent');
-        modalContent.innerHTML = ""; // Tyhjennetään aiempi sisältö
-        const currentMedia = currentGallery[currentIndex];
+    const modalContent = document.getElementById('modalContent');
+    modalContent.innerHTML = ""; // Tyhjennetään aiempi sisältö
+    const currentMedia = currentGallery[currentIndex];
 
-        if (currentMedia.type === "image") {
-            // Luo kuva-elementti
-            const img = document.createElement("img");
-            img.src = currentMedia.src;
-            img.style.width = "100%";
-            img.style.height = "100%";
-            img.style.objectFit = "contain";
-            modalContent.appendChild(img);
-        } else if (currentMedia.type === "video") {
-            // Luo video-elementti
-            const video = document.createElement("video");
-            video.src = currentMedia.src;
-            video.controls = true;
-            video.style.width = "100%";
-            video.style.height = "100%";
-            modalContent.appendChild(video);
-        }
+    if (currentMedia.type === "image") {
+        // Luo kuva-elementti
+        const img = document.createElement("img");
+        img.src = currentMedia.src;
+        img.style.maxWidth = "90vw";  // Maksimikoko 90% näkymän leveydestä
+        img.style.maxHeight = "90vh"; // Maksimikoko 90% näkymän korkeudesta
+        img.style.objectFit = "contain";
+        modalContent.appendChild(img);
+    } else if (currentMedia.type === "video") {
+        // Luo video-elementti
+        const video = document.createElement("video");
+        video.src = currentMedia.src;
+        video.controls = true;
+        video.style.maxWidth = "90vw";  
+        video.style.maxHeight = "90vh";  
+        modalContent.appendChild(video);
     }
+}
+
 
 document.getElementById('modal').addEventListener('click', (event) => {
     if (event.target === event.currentTarget){
